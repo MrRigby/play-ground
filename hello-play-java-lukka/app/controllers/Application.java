47f28d0696bbeb9e322a5d882144f14ea1b2a7c0
Lukasz Kalek <lukasz.kalek@gmail.com>
2014-12-29 22:04:58 +0100

    working with assets

--- hello-play-java-lukka/app/controllers/Application.java
+++ hello-play-java-lukka/app/controllers/Application.java
@@ -4,18 +4,14 @@ import models.Proposal;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
-import play.mvc.Results;
 
 public class Application extends Controller {
     
 	private static final Form<Proposal> proposalForm = Form.form(Proposal.class);
 	
     public static Result index() {
-        return ok(views.html.index.render("Hello Play Framework"));
-    }
-
-    public static Result welcome(String name) {
-        return ok("<h1>Welcome " + name + "</h1>").as("text/html");
+    	Proposal keynote = Proposal.findKeynote();
+        return ok(views.html.index.render(keynote));
     }
 
     public static Result newProposal() {
