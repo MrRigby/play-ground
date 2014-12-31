b610484fe6de7d4bc0127dffa18b47e211da7f5e
Lukasz Kalek <l.kalek@samsung.com>
2014-12-31 10:16:39 +0100

    Async DB read and write

--- hello-play-java-lukka/app/controllers/Application.java
+++ hello-play-java-lukka/app/controllers/Application.java
@@ -2,6 +2,8 @@ package controllers;
 
 import models.Proposal;
 import play.data.Form;
+import play.libs.F.Function;
+import play.libs.F.Promise;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -9,24 +11,35 @@ public class Application extends Controller {
     
 	private static final Form<Proposal> proposalForm = Form.form(Proposal.class);
 	
-    public static Result index() {
-    	Proposal keynote = Proposal.findKeynote();
-        return ok(views.html.index.render(keynote));
+    public static Promise<Result> index() {
+    	Promise<Proposal> keynote = Proposal.findKeynote();
+		Promise<Result> result = keynote.map(new Function<Proposal, Result>() {
+			@Override
+			public Result apply(Proposal keynote) throws Throwable {
+				return ok(views.html.index.render(keynote));
+			}
+		});
+    	return result;
     }
 
     public static Result newProposal() {
     	return ok(views.html.newProposal.render(proposalForm));
     }
     
-    public static Result submitProposal() {
+    public static Promise<Result> submitProposal() {
     	Form<Proposal> submittedFrom = proposalForm.bindFromRequest();
     	if (submittedFrom.hasErrors()) {
-    		return ok(views.html.newProposal.render(submittedFrom));
+    		return Promise.<Result>pure(ok(views.html.newProposal.render(submittedFrom)));
     	} else {
     		Proposal proposal = submittedFrom.get();
-    		proposal.save();
-    		flash("message", "Thanks for submitting a proposal");
-    		return redirect(routes.Application.index());
+    		Promise<Result> result = proposal.asyncSave().map(new Function<Void, Result>() {
+    			@Override
+    			public Result apply(Void arg0) throws Throwable {
+    	    		flash("message", "Thanks for submitting a proposal");
+    	    		return redirect(routes.Application.index());
+    			}
+    		});
+    		return result;
     	}
     }
 }
--- hello-play-java-lukka/app/models/Proposal.java
+++ hello-play-java-lukka/app/models/Proposal.java
@@ -1,12 +1,22 @@
 package models;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.validation.Valid;
 
+import play.Logger;
 import play.data.validation.Constraints.MaxLength;
 import play.data.validation.Constraints.MinLength;
 import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
+import play.libs.Akka;
+import play.libs.F.Function;
+import play.libs.F.Function0;
+import play.libs.F.Promise;
+import scala.concurrent.ExecutionContext;
 
 @Entity
 public class Proposal extends Model {
@@ -37,7 +47,38 @@ public class Proposal extends Model {
 
 	private static Finder<Long, Proposal> find = new Finder<>(Long.class, Proposal.class);
 	
-	public static Proposal findKeynote() {
-		return find.where().eq("type", SessionType.Keynote).findUnique();
+	private static ExecutionContext ctx = Akka.system().dispatchers().lookup("akka.db-dispatcher");
+	
+	public static Promise<Proposal> findKeynote() {
+		return Promise.promise(new Function0<Proposal>() {
+			@Override
+			public Proposal apply() throws Throwable {
+				return find.where().eq("type", SessionType.Keynote).findUnique();
+			}
+		}, ctx ).recover(new Function<Throwable, Proposal>() {
+			@Override
+			public Proposal apply(Throwable t) throws Throwable {
+				Logger.error("Failed to fetch keynote information", t);
+                Proposal s = new Proposal();
+                s.title = "COMING SOON!";
+                s.proposal = "";
+                Speaker speaker = new Speaker();
+                speaker.name = "";
+                speaker.pictureUrl = "";
+                speaker.twitterId = "";
+                s.speaker = speaker;
+                return s;
+			}
+		}, ctx);
+	}
+
+	public Promise<Void> asyncSave() {
+		return Promise.promise(new Function0<Void>() {
+			@Override
+			public Void apply() throws Throwable {
+				save();
+				return null;
+			}
+		}, ctx);
 	}
 }
